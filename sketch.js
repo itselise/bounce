50c25315ecd8bfa33c93bdc0047d8af91c70c65b
itselise <64755359+itselise@users.noreply.github.com>
2022-02-17 09:57:09 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,17 @@
-var x;
-var xSpeed = 1;
+var x = 200;
+var y = 12.1;
+var xSpeed = 0;
+var ySpeed = 1;
 
 function setup() {
   createCanvas(400, 400);
-  x = width/2;
 }
 
 function draw() {
   background(220);
-  circle(x, height/2, 24);
-  x+=xSpeed
+  circle(x, y, 24);
+  y +=ySpeed;
+  x += xSpeed;
+  if(y > height - 12 || y < 12) ySpeed *= -1;
   if(x > width - 12 || x < 12) xSpeed *= -1;
 }
